6ae801ca5cbd2c589fe77eac41ee611d2df8148e
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-02-09 20:16:03 -0300

    change api base url

---
+++ components/form-caracteristicas/index.tsx
@@ -0,0 +1,14 @@
+export function CaracteristicasForm(){
+
+    function handleSubmit(){
+        
+    }
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <label>
+                <input type="text" />
+            </label>
+            <button>agregar</button>
+        </form>
+    </div>
+}
